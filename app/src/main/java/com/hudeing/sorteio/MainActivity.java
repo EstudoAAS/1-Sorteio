17fa299887c4f528e2561fc82b0585f92f86218b
Aller Augusto de Souza <aller.souza@samsung.com>
2018-12-11 15:35:41 -0200

    Add logic

--- app/src/main/java/com/hudeing/sorteio/MainActivity.java
+++ app/src/main/java/com/hudeing/sorteio/MainActivity.java
@@ -2,6 +2,9 @@ package com.hudeing.sorteio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+
+        TextView textoResultado = findViewById(R.id.textoResultado);
+        int x = new Random().nextInt(11);
+        textoResultado.setText("O número selecionado é: " + x );
     }
 }
